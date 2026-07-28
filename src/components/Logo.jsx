3fa6d9e27d3bec3ed1978deb6a673ae82c330b8f
Trisha Caisip <trishacaisip1@gmail.com>
2026-07-29 04:27:52 +0800

    feat: update homepage banner and brand icon

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
-import logoImage from '../../OPH/Logo.png'
+const logoImage = '/images/Icon.png'
 
 const Logo = ({
   className = '',
